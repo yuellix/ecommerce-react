94c5cbd0a26def4b51883ec3c28b631fac651398
Ellica Dagala <dagalaellica72@gmail.com>
2025-08-26 19:30:11 +0800

    Remove node_modules from repo

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
